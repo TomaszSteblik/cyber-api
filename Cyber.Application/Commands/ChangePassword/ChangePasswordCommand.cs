6acf9f80811ce5fabb87561cdfe03dd462fe3c74
TomaszSteblik <tsteblik@gmail.com>
2022-10-30 16:00:06 +0100

    :))

--- Cyber.Application/Commands/ChangePassword/ChangePasswordCommand.cs
+++ Cyber.Application/Commands/ChangePassword/ChangePasswordCommand.cs
@@ -5,11 +5,13 @@ namespace Cyber.Application.Commands.ChangePassword;
 public class ChangePasswordCommand : IRequest
 {
     public Guid UserId { get; set; }
+    public string OldPassword { get; set; }
     public string NewPassword { get; set; }
 
-    public ChangePasswordCommand(string newPassword, Guid userId)
+    public ChangePasswordCommand(string newPassword, Guid userId, string oldPassword)
     {
         NewPassword = newPassword;
         UserId = userId;
+        OldPassword = oldPassword;
     }
 }
--- Cyber.Application/DTOs/Update/ChangePasswordDto.cs
+++ Cyber.Application/DTOs/Update/ChangePasswordDto.cs
@@ -3,4 +3,5 @@ namespace Cyber.Application.DTOs.Update;
 public class ChangePasswordDto
 {
     public string NewPassword { get; set; }
+    public string OldPassword { get; set; }
 }
