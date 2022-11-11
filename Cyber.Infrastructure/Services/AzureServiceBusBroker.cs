2e3de2c2a3ff42f2559df51ca5ce7ca8c0dfbe86
TomaszSteblik <tsteblik@gmail.com>
2022-11-11 10:20:37 +0100

    formt

--- Cyber.Infrastructure/Services/AzureServiceBusBroker.cs
+++ Cyber.Infrastructure/Services/AzureServiceBusBroker.cs
@@ -22,8 +22,8 @@ public class AzureServiceBusBroker : IMessageBroker
             Event = message.GetType().Name,
             Value = message
         };
-        var options = new JsonSerializerOptions() {MaxDepth = 64};
-        var messageBytes = JsonSerializer.SerializeToUtf8Bytes(messageToSerialize,options);
+        var options = new JsonSerializerOptions() { MaxDepth = 64 };
+        var messageBytes = JsonSerializer.SerializeToUtf8Bytes(messageToSerialize, options);
         var messageUtf = new ServiceBusMessage(messageBytes);
         await sender.SendMessageAsync(messageUtf);
     }
